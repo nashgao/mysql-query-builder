da4adbf18a78b50b64f95be03b96ae50ee9e4d8f
nashgao <nashgao1992@gmail.com>
2021-02-10 18:37:37 +1000

    update helper function

--- src/Helper/function.php
+++ src/Helper/function.php
@@ -15,31 +15,36 @@ declare(strict_types=1);
 use Nashgao\MySQL\QueryBuilder\Bean\SplBean;
 
 
-if (! function_exists('array_filter_null_value')) {
-    /**
-     * @param array $array
-     * @return array
-     */
-    function array_filter_null_value(array $array): array
-    {
-        return array_filter(
-            $array,
-            function($item) {
-                return !is_null($item);
-            }
-        );
-    }
-}
-
-if (! function_exists('filterBean')) {
+if (!function_exists('filterBean')) {
     /**
      * Filter the bean and to array with not null
      * @param SplBean $bean
      * @param array $filter
      * @return array
      */
-    function filterBean(SplBean $bean, array $filter = []):array
+    function filterBean(SplBean $bean, array $filter = []): array
     {
+        $validKeys = array_diff_key($bean->toArrayWithMapping(), array_fill_keys($filter, null));
+        if (empty($validKeys)) {
+            return [];
+        }
         return $bean->toArray(array_keys(array_diff_key($bean->toArrayWithMapping(), array_fill_keys($filter, null))), $bean::FILTER_NOT_NULL);
+    }
+}
+
+
+if (!function_exists('arrayFilterNullValue')) {
+    /**
+     * @param array $array
+     * @return array
+     */
+    function arrayFilterNullValue(array $array): array
+    {
+        return array_filter(
+            $array,
+            function ($item) {
+                return !is_null($item);
+            }
+        );
     }
 }
