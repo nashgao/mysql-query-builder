b4e3d4743bbf21590f306d726c5ca2be74530625
nashgao <nashgao1992@gmail.com>
2021-02-10 22:05:50 +1000

    remove redundant comment

--- src/Bean/SplBean.php
+++ src/Bean/SplBean.php
@@ -1,14 +1,4 @@
 <?php
-/**
- * Copyright (C) SPACE Platform PTY LTD - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
- * Written by Nash Gao <nash@spaceplaform.co>
- * @organization Space Platform
- * @project composer
- * @create Created on 2021/1/31 下午1:24
- * @author Nash Gao
- */
 
 declare(strict_types=1);
 
--- src/MySQLModel.php
+++ src/MySQLModel.php
@@ -1,12 +1,4 @@
 <?php
-/**
- * Copyright (C) SPACE Platform PTY LTD - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
- * Written by Nash Gao <nash@spaceplaform.co>
- * @organization Space Platform
- * @author Nash Gao
- */
 
 declare(strict_types=1);
 
