df024cc05122764ea3b06d02076002bfeaa3384a
nashgao <nashgao1992@gmail.com>
2021-05-15 19:37:01 +1000

    make stubs by default in mockery

--- test/Stub/MySQLDaoStub.php
+++ test/Stub/MySQLDaoStub.php
@@ -60,41 +60,50 @@ abstract class MySQLDaoStub
 
         $dao->shouldReceive('value')
             ->with(\Mockery::type(MySQLBean::class))
-            ->andReturn(\Mockery::any());
+            ->andReturn(\Mockery::any())
+            ->byDefault();
 
         $dao->shouldReceive('pluck')
             ->with(\Mockery::type(MySQLBean::class))
-            ->andReturn(static::$bean->toArray());
+            ->andReturn(static::$bean->toArray())
+            ->byDefault();
 
         $dao->shouldReceive('first')
-            ->andReturn(static::$bean->toArray());
+            ->andReturn(static::$bean->toArray())
+            ->byDefault();
 
         $dao->shouldReceive('get')
-            ->andReturn(static::$bean->toArray(), null);
+            ->andReturn(static::$bean->toArray(), null)
+            ->byDefault();
 
         $dao->shouldReceive('getMulti')
             ->with(\Mockery::type(MySQLBean::class))
-            ->andReturn(static::$bean->toArray(), null);
+            ->andReturn(static::$bean->toArray(), null)
+            ->byDefault();
 
         $dao->shouldReceive('getAll')
             ->andReturn([static::$bean->toArray()]);
 
         $dao->shouldReceive('fieldExists')
             ->with(\Mockery::type('string'), \Mockery::any())
-            ->andReturn(true);
+            ->andReturn(true)
+            ->byDefault();
 
         $dao->shouldReceive('find')
             ->with(\Mockery::any())
-            ->andReturn(static::$model);
-
-        $dao->shouldReceive('getFromCache')
-            ->with(\Mockery::any())
-            ->andReturn(static::$model, null);
+            ->andReturn(static::$model)
+            ->byDefault();
 
         // add cache query
+        $dao->shouldReceive('getFromCache')
+            ->with(\Mockery::any())
+            ->andReturn(static::$model, null)
+            ->byDefault();
+
         $dao->shouldReceive('getColumnFromCache')
             ->with(\Mockery::type(MySQLBean::class))
-            ->andReturn(\Mockery::type('array'));
+            ->andReturn(\Mockery::type('array'))
+            ->byDefault();
 
         return $dao;
     }
